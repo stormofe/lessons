f6be3c7e721fcf057e31dfe630589f445ed8ee93
Aleksandra <stormofe@gmail.com>
2021-06-04 11:07:53 +0300

    less 20

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 "use strict";
 let numberOfFilm;
 
@@ -83,9 +84,8 @@ function showMyDB(hidden) {
 		console.log(personalMovieDB);
 	}
 }
+
 showMyDB(personalMovieDB.privat);
-
-
 
 function writeYourGenres() {
 	for(let i = 1; i < 4; i++) {
@@ -95,3 +95,43 @@ function writeYourGenres() {
 }
 
 writeYourGenres(); 
+
+
+
+//========================================================================================================================================================
+
+
+const options = {
+	name: 'test',
+	width: 1200,
+	height: 1000,
+	colors: {
+		border: 'green',
+		bg: 'red'
+	},
+	makeTest: function() {
+		console.log("Test");
+	}
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+console.log(Object.keys(options).length);
+
+let counter = 0; 
+
+for (let key in options) {
+	if (typeof(options[key]) === 'object') {
+		for (let i in options[key]) {
+			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+		} 
+	} else {
+		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+	}
+	counter++;
+}
+
+console.log(counter);
